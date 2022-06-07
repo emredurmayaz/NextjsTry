fd43e5b7279eba3356ef2876977bd2c69e09a135
emredurmayaz <emredurmayaz@hotmail.com>
2022-06-07 16:27:35 +0300

    Dynamic API Routes

--- nextjs-course-api-routes/pages/feedback/index.js
+++ nextjs-course-api-routes/pages/feedback/index.js
@@ -1,12 +1,32 @@
+import { useState } from "react";
+import { Fragment } from "react/cjs/react.production.min";
 import { extractFeedback, buildFeedbackPath } from "../api/feedback";
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedBackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((reponse) => response.json())
+      .then((data) => {
+        setFeedBackData(data.feedback);
+      }); // /api/some-feedback-id
+  }
+
   return (
+      <Fragment>
+          {feedbackData && <p>{feedbackData.email}</p>}
     <ul>
       {props.feedbackItems.map((item) => (
-        <li key={item.key}>{item.text}</li>
+        <li key={item.key}>
+          {item.text}
+          <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+            Show Details
+          </button>
+        </li>
       ))}
     </ul>
+    </Fragment>
   );
 }
 
