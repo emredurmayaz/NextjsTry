2045e1c728378ebcb7c72baee18380b80709d0dd
emredurmayaz <emredurmayaz@hotmail.com>
2022-06-13 16:50:56 +0300

    Rendering Images

--- nextjs-course-app/components/posts/post-detail/post-content.js
+++ nextjs-course-app/components/posts/post-detail/post-content.js
@@ -1,8 +1,41 @@
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 import PostHeader from "./post-header";
 import classes from "./post-header.module.css";
 
+const customRenderers = {
+  // img(image) {
+  //   return (
+  //     <Image
+  //       src={`/images/posts/${post.slug}/${image.src}`}
+  //       alt={image.alt}
+  //       width={600}
+  //       height={300}
+  //     />
+  //   );
+  // },
+  p(paragraph) {
+    const { node } = paragraph;
+
+    if (node.children[0].tagName === "img") {
+      const image = node.children[0];
+
+      return (
+        <div className={classes.image}>
+          <Image
+            src={`/images/posts/${post.slug}/${image.properties.src}`}
+            alt={image.alt}
+            width={600}
+            height={300}
+          />
+        </div>
+      );
+    }
+
+    return <p>{paragraph.children}</p>;
+  },
+};
 
 function PostContent(props) {
   const { post } = props;
@@ -12,7 +45,7 @@ function PostContent(props) {
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
