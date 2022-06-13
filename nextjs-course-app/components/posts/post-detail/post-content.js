aea4032bae66b529a7e71c458d721fc8670e5f9f
emredurmayaz <emredurmayaz@hotmail.com>
2022-06-13 17:37:12 +0300

    Rendering Code Snippets

--- nextjs-course-app/components/posts/post-detail/post-content.js
+++ nextjs-course-app/components/posts/post-detail/post-content.js
@@ -1,20 +1,12 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 import classes from "./post-header.module.css";
 
 const customRenderers = {
-  // img(image) {
-  //   return (
-  //     <Image
-  //       src={`/images/posts/${post.slug}/${image.src}`}
-  //       alt={image.alt}
-  //       width={600}
-  //       height={300}
-  //     />
-  //   );
-  // },
   p(paragraph) {
     const { node } = paragraph;
 
@@ -34,6 +26,17 @@ const customRenderers = {
     }
 
     return <p>{paragraph.children}</p>;
+  },
+
+  code(code) {
+    const { language, value } = code;
+    return (
+      <SyntaxHighlighter
+        style={atomDark}
+        language={language}
+        children={value}
+      />
+    );
   },
 };
 
