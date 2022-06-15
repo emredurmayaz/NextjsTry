e89e4a1aabf8328e4e9cfe8ed0737949e780aebf
emredurmayaz <emredurmayaz@hotmail.com>
2022-06-15 15:08:27 +0300

    try

--- nextjs-newproject/components/header.js
+++ nextjs-newproject/components/header.js
@@ -1,27 +1,52 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-function Header(){
-    return <header>
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/">
-                        Anasayfa
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        Hakkında
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/blog">
-                        Blog
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Anasayfa</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>Hakkında</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <style jsx>
+        {`
+          .header {
+            background-color: white;
+            border-bottom: 1px solid #ccc;
+
+            ul {
+              display: flex;
+
+              li {
+                a {
+                  height: 60px;
+                  display: flex;
+                  align-items: center;
+                  padding: 0 20px;
+                  color: #333;
+                  text-decoration: none;
+                }
+              }
+            }
+          }
+        `}
+      </style>
     </header>
+  );
 }
 
 export default Header;
