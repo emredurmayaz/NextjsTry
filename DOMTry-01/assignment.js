8dab8aad1a4ba6c6faa33e614f5527076d903bcf
emredurmayaz <emredurmayaz@hotmail.com>
2022-06-20 11:19:10 +0300

    Quick Sample for Dom Man.

---
+++ DOMTry-01/assignment.js
@@ -0,0 +1,19 @@
+const taskEl1 = document.getElementById('task-1');
+const taskEl2 = document.querySelector('li');
+
+
+
+taskEl1.style.color = 'white';
+taskEl1.style.backgroundColor = 'black';
+
+
+const docTitle1 = document.querySelector('title');
+docTitle1.textContent = 'Assignment - Solved';
+
+const docHead = document.head;
+const docTitle2 = document.head.querySelector('title');
+
+docTitle2.textContent = 'Assignment - Solved';
+
+const h1txt = document.querySelector('h1');
+h1txt[0].textContent = 'Assignment - Solved';
